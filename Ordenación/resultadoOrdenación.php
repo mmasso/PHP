781ff714548213bab638ba596fe0b4a0d819ddc1
mmasso <mmasso@cifpfbmoll.eu>
2020-10-30 16:08:31 +0100

    done

--- Ordenación/resultadoOrdenación.php
+++ Ordenación/resultadoOrdenación.php
@@ -1,98 +1,99 @@
 <!DOCTYPE html>
 <html lang="en">
+<!-- @author Mateu Massó Grau -->
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css.css">
+  </head>
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-  <link rel="stylesheet" href="css.css">
-</head>
+  <body>
 
-<body>
+    <div class="cards">
+      <div class="card">
+        <h2>Ordenación con PHP</h2>
 
-  <div class="cards">
-    <div class="card">
-      <h2>Búsqueda binaria con PHP</h2>
+        <?php
 
-      <?php
+        $seleccionArray = $_POST['entradaDataOrdenacion'];
+        $method =  $_POST['entradaOrdenacion'];
 
-      $seleccionArray = $_POST['entradaDataOr'];
-      $falloInputs = false;
-      $method =  $_POST['entradaOrdenacion'];
 
-      if (empty($method)){
-        echo "<p> No ha introducido un valor de ordenación </p>";
-        $falloInputs = true;
-      }
-      if ($seleccionArray == "arrayManualOr") {
-        $arraySinTratar =  $_POST['numerosOr'];
-        $array = explode(" ", $arraySinTratar);
-        $arrayString = implode(", ", $array);
-      }
-      if ($seleccionArray == "arrayPredefinidoOr") {
-        $array = array(1, 5, 3, 7, 8, 6, 2, 70, 9, 10);
-        $arrayString = implode(", ", $array);
-      }
-      if ($seleccionArray == "arrayRandomOr") {
-        $arrayLength =  $_POST["longitudOr"];
-        $array = randomArray($arrayLength);
-        $arrayString = implode(", ", $array);
-      }
-      if ($seleccionArray == "defaultOr"){
-        echo "<p> No ha introducido un parametro válido en la aplicación </p>";
-        $falloInputs = true;
-      }
-
-      function randomArray($arrayLength)
-      {
-        $randomArray = array();
-        for ($i = 0; $i < $arrayLength; $i++) {
-            $x = rand(0, 100);
-          $randomArray[$i] = $x;
+        if (empty($method)) {
+          echo "<p> No ha introducido un valor de ordenación </p>";
         }
-        return $randomArray;
-      }
+        if ($seleccionArray == "arrayManualOr") {
+          $arraySinTratar =  $_POST['numerosOr'];
+          $array = explode(" ", $arraySinTratar);
+        }
+        if ($seleccionArray == "arrayPredefinidoOr") {
+          $array = array(1, 5, 3, 7, 8, 6, 2, 70, 9, 10);
+        }
+        if ($seleccionArray == "arrayRandomOr") {
+          $arrayLength =  $_POST["longitudOr"];
+          $array = randomArray($arrayLength);
+        }
+        if ($seleccionArray == "defaultOr") {
+          echo "<p> No ha introducido un parametro válido en la aplicación </p>";
+        }
+        if ($method == "burbuja"){
+          $arrayLenght = count($array);
+          $arrayOrdenado = burbuja($array, $arrayLenght);
+          echo "El array ordenado obtenido por burbuja es: ". implode(", ", $arrayOrdenado);
+        }
+        if ($method == "seleccionDirecta"){
+          $arrayLenght = count($array);
+          $arrayOrdenado = insercionDirecta($array, $arrayLenght);
+          echo "El array ordenado por seleccion Directa obtenido es: ". implode(", ", $arrayOrdenado);
+        }
 
-      function burbuja($A,$n)
-      {
-          for($i=1;$i<$n;$i++)
-          {
-                  for($j=0;$j<$n-$i;$j++)
-                  {
-                          if($A[$j]>$A[$j+1])
-                          {$k=$A[$j+1]; $A[$j+1]=$A[$j]; $A[$j]=$k;}
-                  }
+
+        function randomArray($arrayLength)
+        {
+          $randomArray = array();
+          for ($i = 0; $i < $arrayLength; $i++) {
+            $x = rand(0, 100);
+            $randomArray[$i] = $x;
           }
-   
-        return $A;
-      }
+          return $randomArray;
+        }
 
-      function insercionDirecta($A,$n)
-      {
-   
-          for ($i = 1; $i < $n; $i++)
-          {
-                   $v = $A[$i];
-                   $j = $i - 1;
-                   while ($j >= 0 && $A[$j] > $v)
-                   {
-                            $A[$j + 1] = $A[$j];
-                            $j--;
-                   }
-   
-                   $A[$j + 1] = $v;
+        function burbuja($array, $arrayLenght)
+        {
+          for ($i = 1; $i < $arrayLenght; $i++) {
+            for ($j = 0; $j < $arrayLenght - $i; $j++) {
+              if ($array[$j] > $array[$j + 1]) {
+                $k = $array[$j + 1];
+                $array[$j + 1] = $array[$j];
+                $array[$j] = $k;
+              }
+            }
           }
-   
-          return $A;
-      }
 
-      echo "hola"
+          return $array;
+        }
 
+        function insercionDirecta($array, $arrayLenght)
+        {
+          for ($i = 1; $i < $arrayLenght; $i++) {
+            $v = $array[$i];
+            $j = $i - 1;
+            while ($j >= 0 && $array[$j] > $v) {
+              $array[$j + 1] = $array[$j];
+              $j--;
+            }
 
-      ?>
+            $array[$j + 1] = $v;
+          }
+
+          return $array;
+        }
+        ?>
+        
+      </div>
     </div>
-  </div>
 
-</body>
+  </body>
 
 </html>
