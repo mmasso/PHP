39f4cfc0b459c8eaf3a611d4b02e52c550460a30
mmasso <mmasso@cifpfbmoll.eu>
2020-12-09 13:29:09 +0100

    3

--- CRUD/php-beginner-crud-level-1/index.php
+++ CRUD/php-beginner-crud-level-1/index.php
@@ -28,6 +28,16 @@
         <?php
 // include database connection
 include 'config/database.php';
+
+// PAGINATION VARIABLES
+// page is the current page, if there's nothing set, default is page 1
+$page = isset($_GET['page']) ? $_GET['page'] : 1;
+ 
+// set records or rows of data per page
+$records_per_page = 5;
+ 
+// calculate for the query LIMIT clause
+$from_record_num = ($records_per_page * $page) - $records_per_page;
  
 // delete message prompt will be here
 $action = isset($_GET['action']) ? $_GET['action'] : "";
@@ -38,8 +48,13 @@ if($action=='deleted'){
 }
  
 // select all data
-$query = "SELECT id, name, description, price FROM products ORDER BY id DESC";
+// select data for current page
+$query = "SELECT id, name, description, price FROM products ORDER BY id DESC
+    LIMIT :from_record_num, :records_per_page";
+ 
 $stmt = $con->prepare($query);
+$stmt->bindParam(":from_record_num", $from_record_num, PDO::PARAM_INT);
+$stmt->bindParam(":records_per_page", $records_per_page, PDO::PARAM_INT);
 $stmt->execute();
  
 // this is how to get number of rows returned
@@ -94,6 +109,22 @@ while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
  
 // end table
 echo "</table>";
+
+// PAGINATION
+// count total number of rows
+$query = "SELECT COUNT(*) as total_rows FROM products";
+$stmt = $con->prepare($query);
+ 
+// execute query
+$stmt->execute();
+ 
+// get total rows
+$row = $stmt->fetch(PDO::FETCH_ASSOC);
+$total_rows = $row['total_rows'];
+
+// paginate records
+$page_url="index.php?";
+include_once "paging.php";
      
 }
  
